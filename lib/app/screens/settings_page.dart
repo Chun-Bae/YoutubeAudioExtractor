abfa168c2af69d519726354c6ce40dd31bd9b482
Bull <dbtjrdla2056@gmail.com>
2024-06-20 04:50:42 +0900

    settings page: design Widget

--- lib/app/screens/settings_page.dart
+++ lib/app/screens/settings_page.dart
@@ -7,7 +7,8 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 175, 220, 255),
+        backgroundColor: const Color(0xFF14181B),
+        iconTheme: const IconThemeData(color: Colors.white),
         titleSpacing: 0.0,
         title: const Padding(
           padding: EdgeInsets.only(left: 10.0),
@@ -21,15 +22,104 @@ class SettingsPage extends StatelessWidget {
           ),
         ),
       ),
-      body: const Center(
-        child: Text(
-          '설정 페이지',
-          style: TextStyle(
-            color: Color.fromARGB(255, 0, 0, 0),
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
+      backgroundColor: const Color(0xFF14181B),
+      body: ListView(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(left: 16.0),
+            child: Text(
+              '기타',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-        ),
+          const Divider(
+            color: Color.fromARGB(255, 83, 83, 83),
+            height: 30,
+            thickness: 3.0,
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.help_outline,
+              color: const Color(0xFFFF5963),
+            ),
+            title: Text(
+              '도움말',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {
+              // Navigate to Help Page
+            },
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.feedback_outlined,
+              color: const Color(0xFFFF5963),
+            ),
+            title: Text(
+              '건의사항',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {
+              // Navigate to Feedback Page
+            },
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.privacy_tip_outlined,
+              color: const Color(0xFFFF5963),
+            ),
+            title: Text(
+              '개인정보처리방침',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {
+              // Navigate to Privacy Policy Page
+            },
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.article_outlined,
+              color: const Color(0xFFFF5963),
+            ),
+            title: Text('서비스 약관',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                )),
+            onTap: () {
+              // Navigate to Terms of Service Page
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Center(
+              child: Text(
+                '© 2024. _bull, All rights reserved.',
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
